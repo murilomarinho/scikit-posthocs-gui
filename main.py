8b3f0a0e899eb05c20a15969f9c62f6d7b5d43e1
Murilo <murlou+github@gmail.com>
2019-01-18 15:38:02 +0900

    Added header

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+'''
+(C) Copyright 2019 Murilo Marinho
+
+This file is part of scikit-posthocs-gui.
+
+    scikit-posthocs-gui is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DQ Robotics is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+- Murilo M. Marinho (murilo@nml.t.u-tokyo.ac.jp)
+'''
+
+
 import sys
 import os
 from PyQt5.QtWidgets import *
